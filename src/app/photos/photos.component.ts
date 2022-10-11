c79f4b4ade974b42e9720b9a8e460dc2d409a67c
Akshayjosephtomy <akshayjt007@gmail.com>
2022-10-11 15:13:04 +0530

    photo

--- src/app/photos/photos.component.ts
+++ src/app/photos/photos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-photos',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myapi:ApiService) { 
+    this.fetchData()
+  }
+
+  fetchData=()=>{
+    this.myapi.viewPhoto().subscribe(
+      (data)=>{
+        this.photoData=data
+      }
+    )
+  }
 
   photoData:any = []
 
